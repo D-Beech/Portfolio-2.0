a8855937dd31e757219ab8fc38d8f71de600aba1
D-Beech <135070206+D-Beech@users.noreply.github.com>
2025-08-06 11:20:27 +1200

    Working build, updated vite.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
-import path from "path"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
+  base: "./", // Important for S3 deployment
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
